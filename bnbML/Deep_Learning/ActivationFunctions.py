001dd23a3bc0e56c8fa922dc4fd6da3bcfa414e5
ArtistBanda <chirag.11gomber4@gmail.com>
2019-11-05 23:33:44 +0530

    added Activation Functions

---
+++ bnbML/Deep_Learning/ActivationFunctions.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+def Sigmoid(z):
+    """
+        Sigmoid activation fucntion
+
+        parameters : 
+        -> z (numpy array)
+
+        returns :
+        -> numpy array with shape(z) 
+    """
+    return 1 / (1 + np.exp(-z))
+
+
+def ReLU(z):
+    """
+        ReLU(Rectified Linear Unit) activation fucntion
+
+        parameters :
+        -> z (numpy array)
+
+        returns:
+        -> numpy array with shape(z)
+    """
+    return np.where(z > 0, z, 0)
+
+
+def tanh(z):
+    """
+        tanh activation function
+
+        parameters :
+        -> z (numpy array)
+
+        returns :
+        -> numpy array with shape(z)
+    """
+    return np.tanh(z)
