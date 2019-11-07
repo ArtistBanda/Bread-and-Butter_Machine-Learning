0446de1665470c7bda36e30449c92dd9f6db9540
ArtistBanda <chirag.11gomber4@gmail.com>
2019-11-08 01:19:54 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+long_description = ' '
+
+setuptools.setup(
+    name="bnbML-ArtistBanda",
+    version="0.0.1",
+    author="Chirag Gomber",
+    author_email="chirag.11gomber4@gmail.com",
+    description="Python implementations of some of the fundamental Machine Learning models and algorithms from scratch.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArtistBanda/Bread-and-Butter_Machine-Learning",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license='LICENSE'
+)
