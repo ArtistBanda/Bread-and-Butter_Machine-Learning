d767c5b86b1dc68fbb81214f15196e0e69a27003
ArtistBanda <chirag.11gomber4@gmail.com>
2019-11-21 18:31:28 +0530

    updated Classification.py

--- bnbML/Supervised_Learning/Classification.py
+++ bnbML/Supervised_Learning/Classification.py
@@ -3,6 +3,7 @@ from bnbML.Deep_Learning import ActivationFunctions
 from bnbML.Utils import LossFunctions
 from bnbML.Utils.Metrics import accuracy
 from bnbML.Utils.Plotting import plotLossGraph
+from bnbML.Utils.PreProcessing import normalize
 
 
 class BinaryLogisticRegression(object):
@@ -11,14 +12,19 @@ class BinaryLogisticRegression(object):
         self.bias = 0
         self.history = []
         self.iter_count = 0
+        self.normalize = False
 
     def fit(self, x_train, y_train, epochs=10, learning_rate=0.01):
-        weights = self._initialize_weights(x_train.shape)
+        x_train, y_train = self._checks(x_train, y_train)
+
+        x_train = self._insert_bias(x_train)
+
+        self.weights = self._initialize_weights(x_train.shape)
 
         for x in range(epochs):
 
             y_cap = ActivationFunctions.Sigmoid(
-                np.dot(x_train, weights['weights']) + weights['bias'])
+                np.dot(x_train, self.weights))
 
             self.history.append([LossFunctions.CrossEntropyLoss(
                 y_train, y_cap), accuracy(y_train, y_cap)])
@@ -29,22 +35,28 @@ class BinaryLogisticRegression(object):
 
             N = x_train.shape[1]
 
-            grad_w = (1 / N) * np.dot(x_train.T, (y_cap - y_train))
-            grad_b = (1 / N) * np.sum(y_cap - y_train)
+            grad = (1 / N) * np.dot(x_train.T, (y_cap - y_train))
 
-            weights['weights'] = weights['weights'] - learning_rate * grad_w
-            weights['bias'] = weights['bias'] - learning_rate * grad_b
-
-        self.weights = weights['weights']
-        self.bias = weights['bias']
+            self.weights = self.weights - learning_rate * grad
 
     def predict(self, x):
+        x = self._insert_bias(x)
         return ActivationFunctions.Sigmoid(np.dot(x, self.weights) + self.bias)
 
     def plotLossGraph(self):
         plotLossGraph(self.history, self.iter_count)
 
     def _initialize_weights(self, input_shape):
-        weights = np.zeros((input_shape[1], 1))
-        bias = 0
-        return {'weights': weights, 'bias': bias}
+        return np.zeros((input_shape[1], 1))
+
+    def _insert_bias(self, x):
+        return np.insert(x, 0, 1, axis=1)
+
+    def _checks(self, x, y):
+        if y.shape is not tuple((len(y), 1)):
+            y = np.reshape(y, (y.shape[0], 1))
+
+        if self.normalize is True:
+            x = normalize(x)
+
+        return x, y
