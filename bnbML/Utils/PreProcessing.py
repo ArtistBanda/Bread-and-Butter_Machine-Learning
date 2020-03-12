256bc80d367fd560d4d564db0ddc01e70158137c
ArtistBanda <chirag.11gomber4@gmail.com>
2020-03-12 19:27:39 +0530

    Added train_test_split function in PreProcessing.py

--- bnbML/Utils/PreProcessing.py
+++ bnbML/Utils/PreProcessing.py
@@ -1,4 +1,5 @@
-import numpy as np 
+import numpy as np
+
 
 def normalize(x):
 
@@ -6,3 +7,25 @@ def normalize(x):
     x /= norms[:, np.newaxis]
 
     return x
+
+
+def train_test_split(X, y, split=0.8):
+    """
+        Splits the data into train and test splits
+
+        parameters :
+        -> X, y
+
+        returns:
+        -> (X_train, y_train, X_test, y_test)
+
+    """
+    length = int(len(y.shape[0]) * split)
+
+    X_train = X[:length]
+    y_train = y[:length]
+
+    X_test = X[length:]
+    y_test = y[length:]
+
+    return (X_train, y_train, X_test, y_test)
