0b70a97fce798c1348c82eda8c05bf6253a4f9de
ArtistBanda <chirag.11gomber4@gmail.com>
2020-04-08 13:52:10 +0530

    Updated NeuralNetwork.py

--- bnbML/Deep_Learning/NeuralNetwork.py
+++ bnbML/Deep_Learning/NeuralNetwork.py
@@ -1,6 +1,7 @@
 import numpy as np
 from bnbML.Utils import LossFunctions
 from bnbML.Deep_Learning import ActivationFunctions
+from bnbML.Deep_Learning import BackwardActivationFucntions
 
 
 class NeuralNetwork(object):
@@ -28,21 +29,49 @@ class NeuralNetwork(object):
     def _initialize_parameters(self):
         for i in range(1, len(self.layers)):
             self.parameters['W' +
-                            str(i)] = np.random.randn(self.layers[i][1], self.layers[i - 1][1])
+                            str(i)] = np.random.randn(self.layers[i][1], self.layers[i - 1][1]) * 0.01
             self.parameters['b' + str(i)] = np.zeros((self.layers[i][1], 1))
 
     def _forward_pass(self, A, W, b):
+        cache = (A, W, b)
         Z = np.dot(W, A) + b
-        return Z
+
+        return Z, cache
 
     def _forward_activation_pass(self, A_prev, W, b, activation):
         activation = getattr(ActivationFunctions, activation)
-        Z = self._forward_pass(A_prev, W, b)
-        A = activation(Z)
-        return A
 
-    def _backward_pass(self):
-        pass
+        Z, linear_cache = self._forward_pass(A_prev, W, b)
+        A, activation_cache = activation(Z)
+
+        caches = (linear_cache, activation_cache)
+
+        return A, caches
+
+    def _backward_pass(self, dZ, cache):
+        A_prev, W, b = cache
+        m = A_prev.shape[1]
+
+        dW = np.dot(dZ, A_prev.T) / m
+        db = np.sum(dZ) / m
+        dA_prev = np.dot(W.T, dZ)
+
+        assert (dA_prev.shape == A_prev.shape)
+        assert (dW.shape == W.shape)
+        assert (db.shape == b.shape)
+
+        return (dA_prev, dW, db)
+
+    def _backward_activation_pass(self, dA, caches, activation):
+        linear_cache, activation_cache = caches
+        activation = getattr(ActivationFunctions, activation)
+        backward_activation = getattr(
+            ActivationFunctions, activation + '_backward')
+
+        dZ = backward_activation(dA, activation_cache)
+        dA_prev, dW, db = self._backward_pass(dZ, linear_cache)
+
+        return dA_prev, dW, db
 
     def _compute_cost(self):
         pass
